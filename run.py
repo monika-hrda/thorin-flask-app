7a68d2e0f85354eaa076dd5dad94f0dcedaf5d6e
monika-hrda <mon.hrda@gmail.com>
2022-05-03 18:42:19 +0000

    Pass data from a view to a template

--- run.py
+++ run.py
@@ -17,17 +17,17 @@ def index():     # this function is also called a view
 # bind the path of the decorator to a view called about()
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="About")
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":  # 'main' = name of the default module in Python
