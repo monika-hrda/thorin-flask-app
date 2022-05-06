9cf9df41a98d92ab98859c1707f07f4e7d2da64c
monika-hrda <mon.hrda@gmail.com>
2022-05-06 21:31:05 +0000

    Read from a JSON file

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 
@@ -17,7 +18,10 @@ def index():     # this function is also called a view
 # bind the path of the decorator to a view called about()
 @app.route("/about")
 def about():
-    return render_template("about.html", page_title="About", list_of_numbers=[1, 2, 3])
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact")
